df5ff374b45567d2cfb34fed00f98cbb45003df2
Tosar Ahmed <=>
2025-01-03 20:51:12 +0600

    ordering prcess complete

--- payment/models.py
+++ payment/models.py
@@ -3,18 +3,21 @@ from django.contrib.auth.models import User
 from item.models import Cart, Item
  
 
+class DeliveryAddress(models.Model):
+    cart_id = models.CharField(max_length=55, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    division = models.CharField(max_length=255, blank=True, null=True)
+    district = models.CharField(max_length=255, blank=True, null=True)
+    upazila = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True) 
+    contact_no = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.name}"
     
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-
-    division = models.CharField(max_length=255)
-    district = models.CharField(max_length=255)
-    upazila = models.CharField(max_length=255)
-    address = models.CharField(max_length=255) 
-    contact_no = models.CharField(max_length=255)
-
+    delivery_address = models.ForeignKey(DeliveryAddress, on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     total_price = models.FloatField()
     status = models.CharField(max_length=50, default="Pending")  # e.g., Pending, Completed
@@ -29,7 +32,7 @@ class OrderItem(models.Model):
     price = models.FloatField()  # Store the item's price at the time of order
 
     def __str__(self):
-        return f"{self.quantity} x {self.item.name}"
+        return f"#{self.order.id} - {self.order.user.username} - {self.quantity} x {self.item.name}"
     
  
 
--- payment/views.py
+++ payment/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Cart, Order, OrderItem
+from .models import Cart, Order, OrderItem, DeliveryAddress
 
 @login_required
 def delivery_address(request, subtotal, cartid):
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        division = request.POST.get('division')
+        district = request.POST.get('district')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+
+        cart = get_object_or_404(Cart, user=request.user)
+
+        new_address = DeliveryAddress()
+        new_address.name = name
+        new_address.division = division
+        new_address.district = district
+        new_address.address = address
+        new_address.contact_no = phone
+        new_address.cart_id = cart.id
+        new_address.save()
+
+        return redirect('payment:place_order')
 
     delivery_cost = 50
     total_amount = float(subtotal) + delivery_cost
@@ -28,10 +48,11 @@ def place_order(request):
 
     # Calculate total price
     total_price = sum(item.item.price * item.quantity for item in cart_items)
-
+    total_price += 50 # Delivery Cost
+    # Delivery Address
+    deliver_address_model = DeliveryAddress.objects.filter(cart_id=cart.id).order_by('-id').first()
     # Create the Order
-    order = Order.objects.create(user=request.user, total_price=total_price)
-
+    order = Order.objects.create(user=request.user, total_price=total_price, delivery_address=deliver_address_model,)
     # Create OrderItems for each CartItem
     for cart_item in cart_items:
         OrderItem.objects.create(
@@ -39,15 +60,16 @@ def place_order(request):
             item=cart_item.item,
             quantity=cart_item.quantity,
             price=cart_item.item.price,
+            
         )
 
     # Clear the cart after placing the order
     cart.cart_items.all().delete()
 
     # Redirect to an order confirmation page
-    return redirect('order_confirmation', order_id=order.id)
+    return redirect('payment:order_confirmation', order_id=order.id)
 
 @login_required
 def order_confirmation(request, order_id):
     order = get_object_or_404(Order, id=order_id, user=request.user)
-    return render(request, 'order_confirmation.html', {'order': order})
+    return render(request, 'payment/order_confirmation.html', {'order': order})
